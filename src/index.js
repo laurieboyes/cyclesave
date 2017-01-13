cce201ca660098c61ec137ec39a6c81786e14627
Laurie Boyes <laurie.boyes@ft.com>
2017-01-13 21:30:40 +0000

    honestly can't remember what specifically i wanted react router apart from it was as debug thing but it was awkward so I'm keeping it

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route, Link, browserHistory } from 'react-router';
+
+import Lol from './dev-stuff/Lol';
+import Nope from './dev-stuff/nope';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="lol" component={Lol} />
+    <Route path="*" component={Nope} />
+  </Router>,
   document.getElementById('root')
 );
