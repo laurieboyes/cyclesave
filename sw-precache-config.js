2f6282ac6fd4f593a24ed7bfed9c5e9f6650d76c
Laurie Boyes <laurie.boyes@ft.com>
2017-01-22 21:32:52 +0000

    made the sw cache some api calls

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,10 +1,28 @@
 module.exports = {
-  stripPrefix: 'build/',
-  staticFileGlobs: [
-    'build/*.html',
-    'build/manifest.json',
-    'build/static/**/!(*map*)'
-  ],
-  dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: 'build/service-worker.js'
+    stripPrefix: 'build/',
+    staticFileGlobs: [
+        'build/*.html',
+        'build/manifest.json',
+        'build/static/**/!(*map*)'
+    ],
+    dontCacheBustUrlsMatching: /\.\w{8}\./,
+    swFilePath: 'build/service-worker.js',
+    runtimeCaching: [
+        {
+            urlPattern: /^https?:\/\/apis\.google\.com/,
+            handler: 'cacheFirst'
+        },
+        {
+            urlPattern: /^https?:\/\/api\.tfl\.gov\.uk/,
+            handler: 'cacheFirst'
+        },
+        {
+            urlPattern: /^https?:\/\/transportapi\.com/,
+            handler: 'cacheFirst'
+        },
+        {
+            urlPattern: /^https?:\/\/api\.brfares\.com/,
+            handler: 'cacheFirst'
+        }
+    ]
 };
